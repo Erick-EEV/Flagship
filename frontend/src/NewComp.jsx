85e6247b3dc140b9ae134ca7f810e3c438a81bfc
Erick-EEV <EEVERICKEEV@gmail.com>
2021-01-11 17:36:01 -0500

    Test

---
+++ frontend/src/NewComp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NewComp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
